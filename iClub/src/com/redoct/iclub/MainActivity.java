1f548771ebef31c9989260075ef9376b9b8b6c53
Kevin-ZYF <739578514@qq.com>
2015-01-26 16:26:19 +0800

    conflict test
    1111111

--- iClub/src/com/redoct/iclub/MainActivity.java
+++ iClub/src/com/redoct/iclub/MainActivity.java
@@ -4,6 +4,7 @@ import com.redoct.iclub.R;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -12,6 +13,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Log.e("zyf", "123456.....");
     }
 
 
