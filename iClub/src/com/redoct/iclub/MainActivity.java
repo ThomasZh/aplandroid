0e3764911ad2334bae32f688fb64cac1ae71f92c
Kevin-ZYF <739578514@qq.com>
2015-01-26 16:49:41 +0800

    new
    yyyy

--- iClub/src/com/redoct/iclub/MainActivity.java
+++ iClub/src/com/redoct/iclub/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         Log.i("chengcai","hhaha");
+         Log.i("zyf","123456");
 
     }
 
